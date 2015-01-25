7d6ef43293083ae135a54e2ea48f19927ef78103
Pablo Lopez <lop@Pablos-MacBook-Pro.local>
2015-01-25 16:40:10 -0500

    added info to smoke machine

--- www/js/controllers/SmokeMachineCtrl.js
+++ www/js/controllers/SmokeMachineCtrl.js
@@ -1,16 +1,44 @@
 angular.module('starter')
 .controller('SmokeMachineCtrl', 
-       [ '$scope', 'SoundCloudService', '$ionicLoading' , 'SoundCloudQuery' , 
-function( $scope,   SoundCloudService ,  $ionicLoading,    SoundCloudQuery) {
+       [ '$scope', '$ionicLoading' , 'SoundCloudQuery' , '$ionicModal', '$moment',
+function( $scope ,  $ionicLoading  ,  SoundCloudQuery  ,  $ionicModal ,  $moment) {
 
   var idle = true;
-
   var query =  SoundCloudQuery.queryByUser({user : '981755'});
 
-  $ionicLoading.show({
-    template: 'Loading...'
-  });
   
+  
+
+  function init() {
+    $scope.endOfRecords = false;
+    $scope.hasSearchResults = false;
+
+    $ionicLoading.show({
+      template: 'Loading...'
+    });
+
+    // Create the login modal that we will use later
+    $ionicModal.fromTemplateUrl('templates/info.html', {
+      scope: $scope
+    }).then(function(modal) {
+      $scope.modal = modal;
+    });
+
+  }
+
+  $scope.resultClick = function(result){
+    $scope.info = result;
+
+    result.big_artwork = result.artwork_url.replace('large', 't500x500');
+    result.proper_time = $moment.utc(result.duration).format("HH:mm:ss");
+
+    $scope.modal.show();
+  }
+
+  $scope.closeModal = function(){
+    $scope.modal.hide();  
+  }
+
 
 
   $scope.loadMore = function (){
@@ -25,7 +53,7 @@ function( $scope,   SoundCloudService ,  $ionicLoading,    SoundCloudQuery) {
     }
   }
 
+  init();
 
-}]).controller('PlaylistCtrl', ['$scope', '$stateParams' , function($scope, $stateParams) {
 
 }]);
